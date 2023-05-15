5c1e133b040cade9b15ee00f3f81a94648782881
YeshwanthBE <yeshwanthbe19@gmail.com>
2023-05-15 21:56:35 +0530

    commit

--- Longest_common_subsequence/main.cpp
+++ Longest_common_subsequence/main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 using namespace std;
-int ar[4][2];
+int dr[6][4];
+int Dp_LCS(char *p,char *q,int i=5,int j=3)
+{
+    if(!i||!j)
+        return 0;
+    if(dr[i][j])
+        return dr[i][j];
+    if(p[i-1]==q[j-1])
+        dr[i][j]=Dp_LCS(p,q,i-1,j-1)+1;
+    else
+        dr[i][j]=max(Dp_LCS(p,q,i,j-1),Dp_LCS(p,q,i-1,j));
+    return dr[i][j];
+
+}
+int mr[4][2];
 int Memoi_LCS(char *p,char *q,int i=0,int j=0)
 {
     if(p[i]=='\0'||q[j]=='\0')
         return 0;
-    else if (ar[i][j])
-        return ar[i][j];
+    else if (mr[i][j])
+        return mr[i][j];
     else if(p[i]==q[j])
-        ar[i][j]=Memoi_LCS(p,q,i+1,j+1)+1;
+        mr[i][j]=Memoi_LCS(p,q,i+1,j+1)+1;
     else
-        ar[i][j]=max(Memoi_LCS(p,q,i,j+1),Memoi_LCS(p,q,i+1,j));
-
-    return ar[i][j];
+        mr[i][j]=max(Memoi_LCS(p,q,i,j+1),Memoi_LCS(p,q,i+1,j));
+    return mr[i][j];
 }
 
 
@@ -27,7 +40,7 @@ int Rec_LCS(char *p,char *q,int i=0,int j=0)
 }
 int main()
 {
-    char *p="abcd",*q="bd";
-    cout<<Memoi_LCS(p,q);
+    char *p="abcde",*q="bde";
+    cout<<Dp_LCS(p,q);
     return 0;
 }
