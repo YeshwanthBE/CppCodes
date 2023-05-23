dc22fac7e939fda4736682cf6f17685c4bf041e0
YeshwanthBE <yeshwanthbe19@gmail.com>
2023-05-23 21:56:51 +0530

    commit

--- subset/main.cpp
+++ subset/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-int dp[10][10];
+int dp[11][7];
 void disp(int n,int k)
 {
     for(int i=0;i<n;i++,cout<<endl)
@@ -12,13 +12,16 @@ void disp(int n,int k)
 void subset(int ar[],int n,int k)
 {
     k/=2;
+    for(int i=0;i<=k;i++)
+        dp[0][i]=1;
+    for(int i=0;i<=n;i++)
+        dp[i][0]=0;
     for(int i=1;i<=k;i++)
-        for(int j=1;j<n;j++)
+        for(int j=1;j<=n;j++)
             {
-                if(ar[j]==i)
-                    dp[j][i]=1;
-                else
-                    dp[j][i]=dp[j-1][i-ar[j]]||dp[j-1][i];
+                dp[j][i]=dp[j-1][i];
+                if(i-ar[j-1]>=0)
+                    dp[j][i]=dp[j-1][i-ar[j-1]]||dp[j][i];
             }
     //cout<<dp[n-1][k];
     disp(11,11);
@@ -26,6 +29,6 @@ void subset(int ar[],int n,int k)
 int main()
 {
     int ar[]={3,2,4,19,3,7,13,10,6,11};
-    subset(ar,10,6);
+    subset(ar,10,12);
     return 0;
 }
